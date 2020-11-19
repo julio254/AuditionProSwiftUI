8fee638ad69433a3fe82e31a4440a0e84103993e
Julio Lopez <Juliol2540@gmail.com>
2020-11-19 08:10:34 -0600

    Initial Commit

---
+++ AuditionPro/AuditionProApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuditionProApp.swift
+//  AuditionPro
+//
+//  Created by Julio Lopez on 11/19/20.
+//
+
+import SwiftUI
+
+@main
+struct AuditionProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
